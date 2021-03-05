f96298cb13b1c7c03ccee49bd245008bf0f40e65
Nyamador <dselasi4u@gmail.com>
2021-03-05 21:02:23 +0000

    Added reducer

---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+
+import * as actions from './actionTypes';
+
+let lastId = 0;
+
+export default function reducer(state = [], action){
+    switch (action.type){
+        case actions.BUG_ADDED:
+            return [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ]
+        case action.BUG_RESOLVED:
+            const target = state.filter(bug => bug.id === action.payload.id)
+            target.resolved = action.payload.resolved;
+            return[
+                ...state,
+                target
+            ]
+        case actions.BUG_REMOVED:
+            return state.filter(bug => bug.id !== action.payload.id)
+        default:
+            return state;
+    }
+}
